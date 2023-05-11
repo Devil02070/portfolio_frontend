de81dd79647e756581d15d2d54337f940d5752d6
Ajay Kumar <devilajay1090@gmail.com>
2023-05-11 14:53:06 +0530

    all

--- src/components/views/Members.js
+++ src/components/views/Members.js
@@ -1,5 +1,5 @@
-import React, {useState, useEffect}from 'react'
-import {Link} from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 
 const Members = () => {
   const [user, setUser] = useState([]);
@@ -8,7 +8,7 @@ const Members = () => {
   const [isLoading, setIsLoading] = useState(true);
 
   console.log('hello');
-  const get_members = async()=>{
+  const get_members = async () => {
     const res = await fetch('https://portfolio-backend-hazel.vercel.app/view_users', {
       // method : "GET",
       // headers : {
@@ -20,78 +20,81 @@ const Members = () => {
 
     const data = await res.json();
     console.log(data);
-    
+
     setUser(data.all_users);
     setUserProfile(data.single_user_details);
     setIsLoading(false);
   }
-  useEffect(()=>{
+  useEffect(() => {
     get_members();
-  },[]);
+  }, []);
 
   return (
     <>
-    <div>
       {isLoading ? (
         // Display the loader while the content is loading
-        <div className="loader">Loading...</div>
+        <>
+          <seciton className="loading-screen">
+            <div className="loader"><p className='loader-text text-light'>Loading...</p></div>
+          </seciton>
+        </>
       ) : (
         // Display the content once it's loaded
-        <div>This is the content of my component.</div>
+        <>
+          <section className='members'>
+            <div className="container">
+              <h3 className='rounded'>Users Having portfolio:</h3>
+              <div className="row">
+                {
+                  userprofile.map((elem) => {
+                    return (
+                      // <div key={elem._id} className="user_card m-2 p-4 rounded text-center border m-2">
+                      <div key={elem._id} className="user_card m-2 p-4 text-center m-2">
+
+                        {/* <img src={elem.profile_pic} className='w-100 rounded user-profile-img' height={300}/> */}
+                        <div className="flip-card">
+                          <div className="flip-card-inner">
+                            <div className="flip-card-front">
+                              <img src={elem.profile_pic} className='w-100 user-profile-img' height={300} />
+                            </div>
+                            <div className="flip-card-back">
+                              <p className='text-light fw-light p-3'>{elem.user_desc}</p>
+                            </div>
+                          </div>
+                        </div>
+
+                        <p className='mt-4 fs-5'>{elem.username}</p>
+                        <p className='text-light fw-light'>{elem.profession}</p>
+                        <Link to={`/user/${elem.user_id}`}><button className='btn-red rounded'>View Profile</button></Link>
+                      </div>
+                    )
+                  })
+                }
+              </div>
+            </div>
+          </section>
+
+          <section className='members pb-5'>
+            <div className="container">
+              <h3 className='rounded'>All Members:</h3>
+              <div className="row">
+                {
+                  user.map((single_user) => {
+                    return (
+                      <div key={single_user._id} className="user_card m-2 p-4 rounded text-center border m-2">
+                        <h6>{single_user.f_name + single_user.l_name}</h6>
+                        <p>{single_user.email}</p>
+                        <p>{single_user.phone_no}</p>
+                        <Link to={`/user/${single_user._id}`}><button className='btn-red rounded'>View Portfolio</button></Link>
+                      </div>
+                    )
+                  })
+                }
+              </div>
+            </div>
+          </section>
+        </>
       )}
-    </div>
-    <section className='members'>
-      <div className="container">
-        <h3 className='rounded'>Users Having portfolio:</h3>
-        <div className="row">
-          {
-            userprofile.map((elem)=>{
-              return(
-                // <div key={elem._id} className="user_card m-2 p-4 rounded text-center border m-2">
-                <div key={elem._id} className="user_card m-2 p-4 text-center m-2">
-
-                  {/* <img src={elem.profile_pic} className='w-100 rounded user-profile-img' height={300}/> */}
-                  <div className="flip-card">
-                    <div className="flip-card-inner">
-                      <div className="flip-card-front">
-                        <img src={elem.profile_pic} className='w-100 user-profile-img' height={300}/>
-                      </div>
-                      <div className="flip-card-back">
-                        <p className='text-light fw-light p-3'>{elem.user_desc}</p>
-                      </div>
-                    </div>
-                  </div>
-
-                  <p className='mt-4 fs-5'>{elem.username}</p>
-                  <p className='text-light fw-light'>{elem.profession}</p>
-                <Link to={`/user/${elem.user_id}`}><button className='btn-red rounded'>View Profile</button></Link>
-                </div>
-              )
-            })
-          }
-        </div>
-      </div>
-    </section>
-
-    <section className='members pb-5'>
-      <div className="container">
-          <h3 className='rounded'>All Members:</h3>
-        <div className="row">
-          {
-            user.map((single_user)=>{
-                  return (
-                    <div key={single_user._id} className="user_card m-2 p-4 rounded text-center border m-2">
-                      <h6>{single_user.f_name + single_user.l_name}</h6>
-                      <p>{single_user.email}</p>
-                      <p>{single_user.phone_no}</p>
-                      <Link to={`/user/${single_user._id}`}><button className='btn-red rounded'>View Portfolio</button></Link>
-                    </div>
-                  )
-            })
-          }
-        </div>
-      </div>
-    </section>
     </>
   )
 }
