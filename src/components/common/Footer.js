e7f535b2420d16691977862f82c014f1814308e9
Ajay Kumar <devilajay1090@gmail.com>
2023-05-22 11:19:56 +0530

    all

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -5,21 +5,21 @@ const Footer = () => {
     <>
     <section className='scroll-main'>
         <div className="scroll-left d-flex align-items-center py-2">
-          <div className='mrq-text'>Videographers</div><div className='dash'></div>
-          <div className='mrq-text'>Developers</div><div className='dash'></div>
+          <div className='mrq-text'>Blockchain Developers</div><div className='dash'></div>
+          <div className='mrq-text'>Web Developers</div><div className='dash'></div>
           <div className='mrq-text'>Content Planners</div><div className='dash'></div>
           <div className='mrq-text'>Copywriters</div><div className='dash'></div>
-          <div className='mrq-text'>Designers</div><div className='dash'></div>
+          <div className='mrq-text'>UX/UI Designers</div><div className='dash'></div>
           <div className='mrq-text'>Videographers</div><div className='dash'></div>
-          <div className='mrq-text'>Developers</div><div className='dash'></div>
+          <div className='mrq-text'>Web Developers</div><div className='dash'></div>
         </div>
         <div className="gap"></div>
         <div className="scroll-right d-flex align-items-center py-1">
-          <div className='mrq-text'>Videographers</div><div className='dash'></div>
-          <div className='mrq-text'>Developers</div><div className='dash'></div>
+          <div className='mrq-text'>Blockchain Developers</div><div className='dash'></div>
+          <div className='mrq-text'>Web Developers</div><div className='dash'></div>
           <div className='mrq-text'>Content Planners</div><div className='dash'></div>
           <div className='mrq-text'>Copywriters</div><div className='dash'></div>
-          <div className='mrq-text'>Designers</div><div className='dash'></div>
+          <div className='mrq-text'>UX/UI Designers</div><div className='dash'></div>
           <div className='mrq-text'>Videographers</div><div className='dash'></div>
           <div className='mrq-text'>Developers</div><div className='dash'></div>
         </div>
